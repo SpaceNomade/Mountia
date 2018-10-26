7039505d2edbeed4e2a0183f0f5f03c716c4956c
ToeiAzarothis <patrick.de.almeida89@gmail.com>
2018-10-26 11:21:30 +0200

    Mise a jour des tatouage correction bug

--- test/FichePersonnage/tatouage.php
+++ test/FichePersonnage/tatouage.php
@@ -89,7 +89,7 @@ if ($nomPersonnage == "orochi" || $nomPersonnage == "orochiDragon") {
     $tatouage1Nom = "X";
     $tatouage1Rang = "X";
     $tatouage1Quantité = "X";
-    $tatouageTaille = "X";
+    $tatouage1Taille = "X";
     $tatouage1Emplacement = "X";
     $tatouage1Type = "X";
     $tatouage1Effet = "X";
@@ -172,7 +172,7 @@ if ($nomPersonnage == "orochi" || $nomPersonnage == "orochiDragon") {
     $tatouage1Nom = "Pentacle";
     $tatouage1Rang = "X";
     $tatouage1Quantité = "1";
-    $tatouageTaille = "X";
+    $tatouage1Taille = "X";
     $tatouage1Emplacement = "Torse";
     $tatouage1Type = "Offensif";
     $tatouage1Effet = "+14% de degat de sort";
@@ -256,7 +256,7 @@ if ($nomPersonnage == "orochi" || $nomPersonnage == "orochiDragon") {
     $tatouage1Nom = "X";
     $tatouage1Rang = "X";
     $tatouage1Quantité = "X";
-    $tatouageTaille = "X";
+    $tatouage1Taille = "X";
     $tatouage1Emplacement = "X";
     $tatouage1Type = "X";
     $tatouage1Effet = "X";
@@ -340,7 +340,7 @@ if ($nomPersonnage == "orochi" || $nomPersonnage == "orochiDragon") {
     $tatouage1Nom = "X";
     $tatouage1Rang = "X";
     $tatouage1Quantité = "X";
-    $tatouageTaille = "X";
+    $tatouage1Taille = "X";
     $tatouage1Emplacement = "X";
     $tatouage1Type = "X";
     $tatouage1Effet = "X";
@@ -421,65 +421,85 @@ if ($nomPersonnage == "orochi" || $nomPersonnage == "orochiDragon") {
     echo "<script>console.log('Erreur lors de la selection du personnage');</script>";
 
     $tatouage1Nom = "X";
+    $tatouage1Rang = "X";
     $tatouage1Quantité = "X";
+    $tatouage1Taille = "X";
     $tatouage1Emplacement = "X";
     $tatouage1Type = "X";
     $tatouage1Effet = "X";
 
     $tatouage2Nom = "X";
+    $tatouage2Rang = "X";
     $tatouage2Quantité = "X";
+    $tatouage2Taille = "X";
     $tatouage2Emplacement = "X";
     $tatouage2Type = "X";
     $tatouage2Effet = "X";
 
     $tatouage3Nom = "X";
+    $tatouage3Rang = "X";
     $tatouage3Quantité = "X";
+    $tatouage3Taille = "X";
     $tatouage3Emplacement = "X";
     $tatouage3Type = "X";
     $tatouage3Effet = "X";
 
     $tatouage4Nom = "X";
+    $tatouage4Rang = "X";
     $tatouage4Quantité = "X";
+    $tatouage4Taille = "X";
     $tatouage4Emplacement = "X";
     $tatouage4Type = "X";
     $tatouage4Effet = "X";
 
     $tatouage5Nom = "X";
+    $tatouage5Rang = "X";
     $tatouage5Quantité = "X";
+    $tatouage5Taille = "X";
     $tatouage5Emplacement = "X";
     $tatouage5Type = "X";
     $tatouage5Effet = "X";
 
     $tatouage6Nom = "X";
+    $tatouage6Rang = "X";
     $tatouage6Quantité = "X";
+    $tatouage6Taille = "X";
     $tatouage6Emplacement = "X";
     $tatouage6Type = "X";
     $tatouage6Effet = "X";
 
     $tatouage7Nom = "X";
+    $tatouage7Rang = "X";
     $tatouage7Quantité = "X";
+    $tatouage7Taille = "X";
     $tatouage7Emplacement = "X";
     $tatouage7Type = "X";
     $tatouage7Effet = "X";
 
     $tatouage8Nom = "X";
+    $tatouage8Rang = "X";
     $tatouage8Quantité = "X";
+    $tatouage8Taille = "X";
     $tatouage8Emplacement = "X";
     $tatouage8Type = "X";
     $tatouage8Effet = "X";
 
     $tatouage9Nom = "X";
+    $tatouage9Rang = "X";
     $tatouage9Quantité = "X";
+    $tatouage9Taille = "X";
     $tatouage9Emplacement = "X";
     $tatouage9Type = "X";
     $tatouage9Effet = "X";
 
     $tatouage10Nom = "X";
+    $tatouage10Rang = "X";
     $tatouage10Quantité = "X";
+    $tatouage10Taille = "X";
     $tatouage10Emplacement = "X";
     $tatouage10Type = "X";
     $tatouage10Effet = "X";
-    
+
 }
 
 ?>
